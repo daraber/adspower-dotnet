cdf2448048442a819bd637b6a4585aef0477fceb
David <da6003be-s@student.lu.se>
2024-10-10 19:36:21 +0200

    Refactor: remove inherited props

--- AdsPower.LocalApi/Requests/UpdateProfileRequest.cs
+++ AdsPower.LocalApi/Requests/UpdateProfileRequest.cs
@@ -13,50 +13,6 @@ public record UpdateProfileRequest : ProfileRequest
     public required string UserId { get; init; }
 
     /// <summary>
-    /// The new name of the profile, no more than 100 characters.
-    /// </summary>
-    public string? Name { get; init; }
-
-    /// <summary>
-    /// Domain name of the user's account platform (e.g., facebook.com, amazon.com).
-    /// </summary>
-    public string? DomainName { get; init; }
-
-    /// <summary>
-    /// A list of URLs to open when launching the browser.
-    /// If left empty, the domain name URL will be opened.
-    /// </summary>
-    public string[]? OpenUrls { get; init; }
-
-    /// <summary>
-    /// Username for logging into the account. At least one of username/password or cookie information is required.
-    /// </summary>
-    public string? Username { get; init; }
-
-    /// <summary>
-    /// Password for logging into the account. At least one of username/password or cookie information is required.
-    /// </summary>
-    public string? Password { get; init; }
-
-    /// <summary>
-    /// 2FA key for accounts that require two-factor authentication. 
-    /// This key can be used in an online 2FA code generator or similar authenticator.
-    /// </summary>
-    public string? Fakey { get; init; }
-
-    /// <summary>
-    /// Cookie information for the account. At least one of username/password or cookie information is required.
-    /// </summary>
-    public List<CookieInfo>? Cookie { get; init; }
-
-    /// <summary>
-    /// Whether to filter invalid cookies when verification fails.
-    /// 0: Return invalid cookies (default); 1: Filter out invalid cookies.
-    /// Requires version 2.4.6.6 or above.
-    /// </summary>
-    public int IgnoreCookieError { get; init; } = 0;
-
-    /// <summary>
     /// Proxy IP used for account login (required when using lumauto or oxylabs proxies).
     /// </summary>
     public string? Ip { get; init; }
