c48b1fb160fd19bbd1267ebcd8bda0b2f1745f86
David <da6003be-s@student.lu.se>
2024-10-10 23:25:10 +0200

    Add tests

---
+++ AdsPower.LocalApi.Tests/ApiTestBase.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json;
+using RichardSzalay.MockHttp;
+
+namespace AdsPower.LocalApi.Tests;
+
+public abstract class ApiTestBase
+{
+    private const string Url = "http://localhost";
+
+    protected LocalApiClient CreateMockClient(string endpoint, object content)
+    {
+        var contentString = JsonSerializer.Serialize(content);
+
+        var mockHttp = new MockHttpMessageHandler();
+
+        mockHttp.When($"{Url}/{endpoint}")
+            .Respond("application/json", contentString);
+
+        var httpClient = mockHttp.ToHttpClient();
+        return new LocalApiClient(Url, httpClient);
+    }
+}
---
+++ AdsPower.LocalApi.Tests/LocalApiClient.Tests.cs
@@ -0,0 +1,33 @@
+﻿namespace AdsPower.LocalApi.Tests;
+
+[TestFixture]
+[TestOf(typeof(LocalApiClient))]
+[Parallelizable(ParallelScope.All)]
+public class LocalApiClientTests : ApiTestBase
+{
+    [Test]
+    public async Task GetConnectionStatus_Success()
+    {
+        await TestGetConnectionStatus("status", 0, "success");
+    }
+
+    [Test]
+    public async Task GetConnectionStatus_Failed()
+    {
+        await TestGetConnectionStatus("status", -1, "failed");
+    }
+    
+    private async Task TestGetConnectionStatus(string endpoint, int code, string message)
+    {
+        var content = new { code, msg = message };
+
+        using var apiClient = CreateMockClient(endpoint, content);
+        var response = await apiClient.GetConnectionStatus();
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(response.Code, Is.EqualTo(content.code));
+            Assert.That(response.Message, Is.EqualTo(message));
+        });
+    }
+}
