803e78086b3454f624ea1d0886906eab98539028
David <da6003be-s@student.lu.se>
2024-10-10 19:21:58 +0200

    Refactor

--- AdsPower.LocalApi/IGroupApi.cs
+++ AdsPower.LocalApi/IGroupApi.cs
@@ -7,5 +7,4 @@ public interface IGroupApi
     public Task CreateAsync(GroupRequest request, CancellationToken cancellationToken = default);
     public Task UpdateAsync(UpdateGroupRequest request, CancellationToken cancellationToken = default);
     public Task QueryAsync(QueryGroupRequest request, CancellationToken cancellationToken = default);
-    
 }
