8eb180ea001bd1bd31bdfbbb5c5b94b3d7ff1235
David <da6003be-s@student.lu.se>
2024-10-11 00:18:44 +0200

    Document the properties

--- AdsPower.LocalApi/Models/BrowserData.cs
+++ AdsPower.LocalApi/Models/BrowserData.cs
@@ -2,14 +2,26 @@
 
 namespace AdsPower.LocalApi.Models;
 
+/// <summary>
+/// Represents the data related to browser automation interfaces.
+/// </summary>
 public record BrowserData
 {
+    /// <summary>
+    /// Gets the websocket connections for automation tools (e.g., Selenium, Puppeteer).
+    /// </summary>
     [JsonPropertyName("ws")]
     public Dictionary<string, string>? Websockets { get; init; }
-    
+
+    /// <summary>
+    /// Gets the debug port for the browser instance. Required for operation.
+    /// </summary>
     [JsonPropertyName("debug_port")]
     public required string DebugPort { get; init; }
-    
+
+    /// <summary>
+    /// Gets the file path of the WebDriver executable. Required for operation.
+    /// </summary>
     [JsonPropertyName("webdriver")]
     public required string WebDriver { get; init; }
 }
