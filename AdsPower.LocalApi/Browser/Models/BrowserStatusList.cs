9f125e863466f1c328969789d9a46bb79b7544b7
David <da6003be-s@student.lu.se>
2024-10-11 15:40:13 +0200

    Refactor

--- AdsPower.LocalApi/Browser/Models/BrowserStatusList.cs
+++ AdsPower.LocalApi/Browser/Models/BrowserStatusList.cs
@@ -1,11 +1,12 @@
 ﻿using System.Text.Json.Serialization;
+using AdsPower.LocalApi.Responses;
 
 namespace AdsPower.LocalApi.Browser.Models;
 
 /// <summary>
 /// Represents a list of browser statuses.
 /// </summary>
-public class BrowserStatusList
+public record BrowserStatusList : LocalApiResponse
 {
     [JsonPropertyName("list")] public required List<BrowserData> Browsers { get; init; }
 }
