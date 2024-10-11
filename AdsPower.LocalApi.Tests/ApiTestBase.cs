e2adcba8ba4b535b1ad9c739865793c338aff093
David <da6003be-s@student.lu.se>
2024-10-11 12:12:35 +0200

    More test helpers

--- AdsPower.LocalApi.Tests/ApiTestBase.cs
+++ AdsPower.LocalApi.Tests/ApiTestBase.cs
@@ -6,8 +6,40 @@ namespace AdsPower.LocalApi.Tests;
 public abstract class ApiTestBase
 {
     private const string Url = "http://localhost";
+    
+    
+    protected async Task<TResponse> MockResponse<TRequest, TResponse>(
+        string endpoint,
+        Func<LocalApiClient, Func<TRequest, CancellationToken, Task<TResponse>>> call,
+        TRequest request,
+        object responseContent,
+        CancellationToken cancellationToken = default
+    )
+    {
+        using var mockApiClient = CreateMockClient(endpoint, responseContent);
+        var apiFunction = call(mockApiClient);
 
-    protected LocalApiClient CreateMockClient(string endpoint, object content)
+        var apiResponse = await apiFunction(request, cancellationToken);
+        return apiResponse;
+    }
+    
+    protected void TestCancellationToken<TRequest, TResponse>(
+        string endpoint,
+        Func<LocalApiClient, Func<TRequest, CancellationToken, Task<TResponse>>> call,
+        TRequest request,
+        object responseContent
+    )
+    {
+        using var cancellationTokenSource = new CancellationTokenSource();
+        cancellationTokenSource.Cancel();
+
+        Assert.ThrowsAsync<TaskCanceledException>(async () =>
+        {
+            await MockResponse(endpoint, call, request, responseContent, cancellationTokenSource.Token);
+        });
+    }
+    
+    private LocalApiClient CreateMockClient(string endpoint, object content)
     {
         var contentString = JsonSerializer.Serialize(content);
 
@@ -15,7 +47,7 @@ public abstract class ApiTestBase
 
         mockHttp.When($"{Url}/{endpoint}")
             .Respond("application/json", contentString);
-        
+
         return new LocalApiClient(Url, mockHttp);
     }
 }
