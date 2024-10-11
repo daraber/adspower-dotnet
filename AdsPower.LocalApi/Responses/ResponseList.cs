021e6fe3077a30585f8bf1019d154faebfe41d4a
David <da6003be-s@student.lu.se>
2024-10-11 16:25:54 +0200

    Remove

--- AdsPower.LocalApi/Responses/ResponseList.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Text.Json.Serialization;
-
-namespace AdsPower.LocalApi.Responses;
-
-public class ResponseList<T>
-{
-    [JsonPropertyName("list")]
-    public required List<T> List { get; init; }
-}
