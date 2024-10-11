2e63089b8af49a8552cb1f2190311f0f2732dcf4
David <da6003be-s@student.lu.se>
2024-10-11 16:25:27 +0200

    Group API: Test scaffolding

---
+++ AdsPower.LocalApi.Tests/GroupApi.Tests.cs
@@ -0,0 +1,13 @@
+﻿using AdsPower.LocalApi.Browser;
+using AdsPower.LocalApi.Tests.Internal;
+
+namespace AdsPower.LocalApi.Tests;
+
+[TestFixture]
+[TestOf(typeof(BrowserApi))]
+[Parallelizable(ParallelScope.All)]
+[MockTest]
+public class GroupApiTests : ApiTestBase
+{
+    
+}
