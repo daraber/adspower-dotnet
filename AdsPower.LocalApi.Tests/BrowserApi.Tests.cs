d291dee0dfc0f00a80281cb45503644e6001d646
David <da6003be-s@student.lu.se>
2024-10-11 12:12:49 +0200

    Use test helpers

--- AdsPower.LocalApi.Tests/BrowserApi.Tests.cs
+++ AdsPower.LocalApi.Tests/BrowserApi.Tests.cs
@@ -1,4 +1,5 @@
 ﻿using AdsPower.LocalApi.Requests;
+using AdsPower.LocalApi.Responses;
 
 namespace AdsPower.LocalApi.Tests;
 
@@ -8,12 +9,12 @@ namespace AdsPower.LocalApi.Tests;
 [Category("Mock")]
 public class BrowserApiTests : ApiTestBase
 {
-    // TODO: Test cancellation token support
-    
     [Test]
     public async Task Start_Success()
     {
-        var content = new
+        var request = new StartBrowserRequest { UserId = Guid.NewGuid().ToString() };
+
+        var response = new
         {
             code = 0,
             data = new
@@ -29,20 +30,21 @@ public class BrowserApiTests : ApiTestBase
             msg = "success"
         };
 
-        using var apiClient = CreateMockClient("api/v1/browser/start", content);
-        var browserApi = apiClient.Browser;
-
-        var request = new StartBrowserRequest { UserId = Guid.NewGuid().ToString() };
-        var result = await browserApi.StartAsync(request);
+        var result = await MockResponse<StartBrowserRequest, StartBrowserResponse>(
+            "api/v1/browser/start",
+            apiClient => apiClient.Browser.StartAsync,
+            request,
+            response
+        );
 
         Assert.Multiple(() =>
         {
-            Assert.That(result.Code, Is.EqualTo(content.code));
-            Assert.That(result.Message, Is.EqualTo(content.msg));
+            Assert.That(result.Code, Is.EqualTo(response.code));
+            Assert.That(result.Message, Is.EqualTo(response.msg));
 
             Assert.That(result.Data, Is.Not.Null);
-            Assert.That(result.Data?.DebugPort, Is.EqualTo(content.data.debug_port));
-            Assert.That(result.Data?.WebDriver, Is.EqualTo(content.data.webdriver));
+            Assert.That(result.Data?.DebugPort, Is.EqualTo(response.data.debug_port));
+            Assert.That(result.Data?.WebDriver, Is.EqualTo(response.data.webdriver));
 
             Assert.That(result.Data?.Websockets, Is.Not.Null
                 .And.Count.EqualTo(2)
@@ -55,28 +57,47 @@ public class BrowserApiTests : ApiTestBase
     [Test]
     public async Task Start_Failed()
     {
-        var content = new
+        var request = new StartBrowserRequest { UserId = Guid.NewGuid().ToString() };
+
+        var response = new
         {
             code = -1,
             data = new { },
             msg = "failed"
         };
 
-        using var apiClient = CreateMockClient("api/v1/browser/start", content);
-        var browserApi = apiClient.Browser;
-
-        var request = new StartBrowserRequest
-        {
-            UserId = "xxxx"
-        };
-
-        var result = await browserApi.StartAsync(request);
+        var result = await MockResponse<StartBrowserRequest, StartBrowserResponse>(
+            "api/v1/browser/start",
+            apiClient => apiClient.Browser.StartAsync,
+            request,
+            response
+        );
 
         Assert.Multiple(() =>
         {
-            Assert.That(result.Code, Is.EqualTo(content.code));
+            Assert.That(result.Code, Is.EqualTo(response.code));
             Assert.That(result.Data, Is.Null);
-            Assert.That(result.Message, Is.EqualTo(content.msg));
+            Assert.That(result.Message, Is.EqualTo(response.msg));
         });
+    }
+
+    [Test]
+    public void Start_Canceled()
+    {
+        var request = new StartBrowserRequest { UserId = Guid.NewGuid().ToString() };
+
+        var response = new
+        {
+            code = -1,
+            data = new { },
+            msg = "failed"
+        };
+
+        TestCancellationToken<StartBrowserRequest, StartBrowserResponse>(
+            "api/v1/browser/start",
+            apiClient => apiClient.Browser.StartAsync,
+            request,
+            response
+        );
     }
 }
