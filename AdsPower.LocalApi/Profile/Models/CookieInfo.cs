087e9863d7f9899642835620dd671a08d183b17d
David <da6003be-s@student.lu.se>
2024-10-12 17:57:26 +0200

    Add documentation

--- AdsPower.LocalApi/Profile/Models/CookieInfo.cs
+++ AdsPower.LocalApi/Profile/Models/CookieInfo.cs
@@ -1,14 +1,38 @@
 namespace AdsPower.LocalApi.Profile.Models;
 
+
 /// <summary>
-/// Represents a single cookie's information.
+/// Represents detailed information about a single cookie.
 /// </summary>
 public record CookieInfo
 {
+    /// <summary>
+    /// Gets the domain associated with the cookie.
+    /// </summary>
     public string Domain { get; init; } = string.Empty;
+
+    /// <summary>
+    /// Gets the path associated with the cookie.
+    /// </summary>
     public string Path { get; init; } = "/";
+
+    /// <summary>
+    /// Gets the SameSite attribute of the cookie, indicating the same-site policy.
+    /// </summary>
     public string SameSite { get; init; } = "unspecified";
+
+    /// <summary>
+    /// Gets a value indicating whether the cookie is secure.
+    /// </summary>
     public bool Secure { get; init; } = true;
+
+    /// <summary>
+    /// Gets the value of the cookie.
+    /// </summary>
     public string Value { get; init; } = string.Empty;
+
+    /// <summary>
+    /// Gets the unique identifier of the cookie.
+    /// </summary>
     public int Id { get; init; }
 }
