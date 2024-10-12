1945c799c69f0ca1f4f252bd020d846950994fa9
David <da6003be-s@student.lu.se>
2024-10-12 17:33:59 +0200

    Optimize imports

--- AdsPower.LocalApi.Tests/Internal/MockTestAttribute.cs
+++ AdsPower.LocalApi.Tests/Internal/MockTestAttribute.cs
@@ -1,6 +1,4 @@
 ﻿namespace AdsPower.LocalApi.Tests.Internal;
 
-using NUnit.Framework;
-
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
 internal class MockTestAttribute : CategoryAttribute;
