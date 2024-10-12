40f283e9527fec7576c4d36d26635027d84f7ca9
David <da6003be-s@student.lu.se>
2024-10-12 22:42:44 +0200

    Implement LocalApiListResponse

--- AdsPower.LocalApi/Application/Models/ApplicationList.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Text.Json.Serialization;
-
-namespace AdsPower.LocalApi.Application.Models;
-
-public record ApplicationList
-{
-    [JsonPropertyName("list")]
-    public required List<Application> Applications { get; init; } 
-}
--- AdsPower.LocalApi/Application/Responses/ApplicationListResponse.cs
+++ AdsPower.LocalApi/Application/Responses/ApplicationListResponse.cs
@@ -1,12 +1,11 @@
 ﻿using System.Text.Json.Serialization;
-using AdsPower.LocalApi.Application.Models;
 using AdsPower.LocalApi.Internal;
 using AdsPower.LocalApi.Shared;
 
 namespace AdsPower.LocalApi.Application.Responses;
 
-public record ApplicationListResponse : LocalApiResponse<ApplicationList>
+public record ApplicationListResponse : LocalApiListResponse<Models.Application>
 {
-    [JsonConverter(typeof(EmptyObjectToNullConverter<ApplicationList>))]
-    public override ApplicationList? Data { get; init; }
+    [JsonConverter(typeof(EmptyObjectToNullConverter<Models.Application>))]
+    public override LocalApiList<Models.Application>? Data { get; init; }
 }
