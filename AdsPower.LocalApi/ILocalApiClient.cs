6ba6e0f3562e64fb8d22f0f901c9fe25a1eb22ec
David <da6003be-s@student.lu.se>
2024-10-12 21:57:16 +0200

    ILocalAPiClient: Define 'GetAsync' & 'PostAsync'

--- AdsPower.LocalApi/ILocalApiClient.cs
+++ AdsPower.LocalApi/ILocalApiClient.cs
@@ -1,3 +1,4 @@
+using AdsPower.LocalApi.Internal;
 using AdsPower.LocalApi.Shared;
 
 namespace AdsPower.LocalApi;
@@ -5,4 +6,10 @@ namespace AdsPower.LocalApi;
 public interface ILocalApiClient
 {
     Task<LocalApiResponse> GetConnectionStatusAsync(CancellationToken cancellationToken = default);
+
+    Task<T> GetAsync<T>(string path, IQueryParameterizeable request, CancellationToken cancellationToken = default)
+        where T : LocalApiResponse;
+
+    Task<T> PostAsync<T>(string path, object request, CancellationToken cancellationToken = default)
+        where T : LocalApiResponse;
 }
