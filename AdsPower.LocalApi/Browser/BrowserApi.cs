052833fe25e8717796cff7808bb5dd018dcd662a
David <da6003be-s@student.lu.se>
2024-10-13 20:56:37 +0200

    Add overloads with only required property

--- AdsPower.LocalApi/Browser/BrowserApi.cs
+++ AdsPower.LocalApi/Browser/BrowserApi.cs
@@ -6,6 +6,14 @@ namespace AdsPower.LocalApi.Browser;
 
 public class BrowserApi(ILocalApiClient apiClient) : IBrowserApi
 {
+    #region StartAsync
+
+    public async Task<StartBrowserResponse> StartAsync(string userId, CancellationToken cancellationToken = default)
+    {
+        var request = new StartBrowserRequest { UserId = userId };
+        return await StartAsync(request, cancellationToken);
+    }
+
     public async Task<StartBrowserResponse> StartAsync(
         StartBrowserRequest request,
         CancellationToken cancellationToken = default
@@ -15,10 +23,30 @@ public class BrowserApi(ILocalApiClient apiClient) : IBrowserApi
         return await apiClient.GetAsync<StartBrowserResponse>(path, request, cancellationToken);
     }
 
+    #endregion
+
+    #region StopAsync
+
+    public async Task<LocalApiResponse> StopAsync(string userId, CancellationToken cancellationToken = default)
+    {
+        var request = new BrowserRequest { UserId = userId };
+        return await StopAsync(request, cancellationToken);
+    }
+
     public async Task<LocalApiResponse> StopAsync(BrowserRequest request, CancellationToken cancellationToken = default)
     {
         const string path = "/api/v1/browser/stop";
         return await apiClient.GetAsync<LocalApiResponse>(path, request, cancellationToken);
+    }
+
+    #endregion
+
+    #region GetStatusAsync
+
+    public Task<BrowserStatusResponse> GetStatusAsync(string userId, CancellationToken cancellationToken = default)
+    {
+        var request = new BrowserRequest { UserId = userId };
+        return GetStatusAsync(request, cancellationToken);
     }
 
     public Task<BrowserStatusResponse> GetStatusAsync(
@@ -30,6 +58,16 @@ public class BrowserApi(ILocalApiClient apiClient) : IBrowserApi
         return apiClient.GetAsync<BrowserStatusResponse>(path, request, cancellationToken);
     }
 
+    #endregion
+
+    #region GetStatusListAsync
+
+    public Task<BrowserStatusListResponse> GetStatusListAsync(string userId, CancellationToken cancellationToken = default)
+    {
+        var request = new BrowserRequest { UserId = userId };
+        return GetStatusListAsync(request, cancellationToken);
+    }
+    
     public Task<BrowserStatusListResponse> GetStatusListAsync(
         BrowserRequest request,
         CancellationToken cancellationToken = default
@@ -38,4 +76,6 @@ public class BrowserApi(ILocalApiClient apiClient) : IBrowserApi
         const string path = "/api/v1/browser/local-active";
         return apiClient.GetAsync<BrowserStatusListResponse>(path, request, cancellationToken);
     }
+
+    #endregion
 }
