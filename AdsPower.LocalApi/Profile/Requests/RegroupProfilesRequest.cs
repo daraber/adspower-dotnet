fd889aac7c9f36f30315a8b6acd9545f71dcafe6
David <da6003be-s@student.lu.se>
2024-10-13 19:37:44 +0200

    Refactor

--- AdsPower.LocalApi/Profile/Requests/RegroupProfilesRequest.cs
+++ AdsPower.LocalApi/Profile/Requests/RegroupProfilesRequest.cs
@@ -8,31 +8,10 @@ public record RegroupProfilesRequest
     /// <summary>
     /// List of user IDs to be regrouped. This is a required parameter.
     /// </summary>
-    public List<string> UserIds { get; init; }
+    public required List<string> UserIds { get; init; }
 
     /// <summary>
     /// The ID of the group to which the profiles will be moved. This is a required parameter.
     /// </summary>
-    public string GroupId { get; init; }
-
-    /// <summary>
-    /// Initializes a new instance of the <see cref="RegroupProfilesRequest"/> class.
-    /// </summary>
-    /// <param name="userIds">List of user IDs to be regrouped.</param>
-    /// <param name="groupId">The ID of the target group.</param>
-    public RegroupProfilesRequest(List<string> userIds, string groupId)
-    {
-        if (userIds == null || userIds.Count == 0 || userIds.Count > 100)
-        {
-            throw new ArgumentException("UserIds must contain between 1 and 100 IDs.");
-        }
-
-        if (string.IsNullOrWhiteSpace(groupId))
-        {
-            throw new ArgumentException("GroupId is required.");
-        }
-
-        UserIds = userIds;
-        GroupId = groupId;
-    }
+    public required string GroupId { get; init; }
 }
