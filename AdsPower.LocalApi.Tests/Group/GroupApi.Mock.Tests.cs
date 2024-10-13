3e20d7d5c10526fd7fe98c019661bb0de97ea4c9
David <da6003be-s@student.lu.se>
2024-10-13 18:56:27 +0200

    Tests: add remaining group API tests

--- AdsPower.LocalApi.Tests/Group/GroupApi.Mock.Tests.cs
+++ AdsPower.LocalApi.Tests/Group/GroupApi.Mock.Tests.cs
@@ -1,7 +1,9 @@
 ﻿using AdsPower.LocalApi.Group;
 using AdsPower.LocalApi.Group.Requests;
 using AdsPower.LocalApi.Group.Responses;
+using AdsPower.LocalApi.Shared;
 using AdsPower.LocalApi.Tests.Internal;
+using GroupModel = AdsPower.LocalApi.Group.Models.Group;
 
 namespace AdsPower.LocalApi.Tests.Group;
 
@@ -11,6 +13,8 @@ namespace AdsPower.LocalApi.Tests.Group;
 [MockTest]
 public class GroupApiMockTests : ApiTestBase
 {
+    #region CreateAsync
+
     [Test]
     public async Task Create_Success()
     {
@@ -21,7 +25,7 @@ public class GroupApiMockTests : ApiTestBase
             remark = "group-remark"
         };
 
-        var resultData = await MockSuccessResponse<GroupRequest, CreateGroupResponse, LocalApi.Group.Models.Group>(
+        var resultData = await MockSuccessResponse<GroupRequest, CreateGroupResponse, GroupModel>(
             "/api/v1/group/create",
             apiClient => apiClient.Group.CreateAsync,
             new GroupRequest { GroupName = Guid.NewGuid().ToString() },
@@ -37,22 +41,106 @@ public class GroupApiMockTests : ApiTestBase
     }
 
     [Test]
-    public async Task Create_Failed()
+    public async Task Create_Failed() => await MockFailedResponse<GroupRequest, CreateGroupResponse>(
+        "/api/v1/group/create",
+        apiClient => apiClient.Group.CreateAsync,
+        new GroupRequest { GroupName = Guid.NewGuid().ToString() }
+    );
+
+    [Test]
+    public void Create_Canceled() => TestCancellationToken<GroupRequest, CreateGroupResponse>(
+        "/api/v1/group/create",
+        apiClient => apiClient.Group.CreateAsync,
+        new GroupRequest { GroupName = Guid.NewGuid().ToString() }
+    );
+
+    #endregion
+
+    #region UpdateAsync
+
+    [Test]
+    public async Task Update_Success() => await MockSuccessResponse<UpdateGroupRequest, LocalApiResponse>(
+        "/api/v1/group/update",
+        apiClient => apiClient.Group.UpdateAsync,
+        new UpdateGroupRequest { GroupName = "group-name-1", GroupId = "group-id-1" }
+    );
+
+    [Test]
+    public async Task Update_Failed() => await MockFailedResponse<UpdateGroupRequest, LocalApiResponse>(
+        "/api/v1/group/update",
+        apiClient => apiClient.Group.UpdateAsync,
+        new UpdateGroupRequest { GroupName = "group-name-1", GroupId = "group-id-1" }
+    );
+
+    [Test]
+    public void Update_Canceled() => TestCancellationToken<UpdateGroupRequest, LocalApiResponse>(
+        "/api/v1/group/create",
+        apiClient => apiClient.Group.UpdateAsync,
+        new UpdateGroupRequest { GroupName = "group-name-1", GroupId = "group-id-1" }
+    );
+
+    #endregion
+
+    #region ListAsync
+
+    [Test]
+    public async Task ListAsync_Success()
     {
-        await MockFailedResponse<GroupRequest, CreateGroupResponse>(
-            "/api/v1/group/create",
-            apiClient => apiClient.Group.CreateAsync,
-            new GroupRequest { GroupName = Guid.NewGuid().ToString() }
-        );
+        var responseData = new
+        {
+            list = new[]
+            {
+                new
+                {
+                    group_id = "group-id-1",
+                    group_name = "group-name-1",
+                    remark = "group-remark-1"
+                },
+                new
+                {
+                    group_id = "group-id-2",
+                    group_name = "group-name-2",
+                    remark = "group-remark-2"
+                }
+            },
+            page = 1,
+            page_size = 10
+        };
+
+        var resultData =
+            await MockSuccessResponse<ListGroupsRequest, GroupListResponse, LocalApiList<GroupModel>>(
+                "/api/v1/group/list",
+                apiClient => apiClient.Group.ListAsync,
+                new ListGroupsRequest { GroupName = "group-name-1" },
+                responseData
+            );
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(resultData.List, Is.Not.Null.Or.Empty);
+            Assert.That(resultData.List, Has.Count.EqualTo(2));
+            Assert.That(resultData.List[0].GroupId, Is.EqualTo(responseData.list[0].group_id));
+            Assert.That(resultData.List[0].GroupName, Is.EqualTo(responseData.list[0].group_name));
+            Assert.That(resultData.List[0].Remark, Is.EqualTo(responseData.list[0].remark));
+            Assert.That(resultData.List[1].GroupId, Is.EqualTo(responseData.list[1].group_id));
+            Assert.That(resultData.List[1].GroupName, Is.EqualTo(responseData.list[1].group_name));
+            Assert.That(resultData.List[1].Remark, Is.EqualTo(responseData.list[1].remark));
+        });
     }
 
     [Test]
-    public void Create_Canceled()
-    {
-        TestCancellationToken<GroupRequest, CreateGroupResponse>(
-            "/api/v1/group/create",
-            apiClient => apiClient.Group.CreateAsync,
-            new GroupRequest { GroupName = Guid.NewGuid().ToString() }
-        );
-    }
+    public async Task ListAsync_Failed() => await MockFailedResponse<ListGroupsRequest, GroupListResponse>(
+        "/api/v1/group/list",
+        apiClient => apiClient.Group.ListAsync,
+        new ListGroupsRequest { GroupName = Guid.NewGuid().ToString() }
+    );
+
+    [Test]
+    public void ListAsync_Canceled() => TestCancellationToken<ListGroupsRequest, GroupListResponse>(
+        "/api/v1/group/list",
+        apiClient => apiClient.Group.ListAsync,
+        new ListGroupsRequest { GroupName = Guid.NewGuid().ToString() }
+    );
+
+    #endregion
 }
