9da3e8d642fb9c82fe92e77af1ff8d6f51ab8c25
David <da6003be-s@student.lu.se>
2024-10-13 19:35:50 +0200

    Refactor

--- AdsPower.LocalApi/Profile/Requests/DeleteProfileRequest.cs
+++ AdsPower.LocalApi/Profile/Requests/DeleteProfileRequest.cs
@@ -8,19 +8,5 @@ public record DeleteProfileRequest
     /// <summary>
     /// List of user IDs to be deleted. This is a required parameter.
     /// </summary>
-    public List<string> UserIds { get; init; }
-
-    /// <summary>
-    /// Initializes a new instance of the <see cref="DeleteProfileRequest"/> class.
-    /// </summary>
-    /// <param name="userIds">List of user IDs to be deleted.</param>
-    public DeleteProfileRequest(List<string> userIds)
-    {
-        if (userIds == null || userIds.Count == 0 || userIds.Count > 100)
-        {
-            throw new ArgumentException("UserIds must contain between 1 and 100 IDs.");
-        }
-
-        UserIds = userIds;
-    }
+    public required List<string> UserIds { get; init; }
 }
