9e1d91c478b968060b099217c69021797095e8f8
David <da6003be-s@student.lu.se>
2024-10-13 21:03:24 +0200

    Add overloads

--- AdsPower.LocalApi/Group/GroupApi.cs
+++ AdsPower.LocalApi/Group/GroupApi.cs
@@ -6,10 +6,32 @@ namespace AdsPower.LocalApi.Group;
 
 public class GroupApi(ILocalApiClient apiClient) : IGroupApi
 {
+    #region CreateAsync
+
+    public Task<CreateGroupResponse> CreateAsync(string groupName, CancellationToken cancellationToken = default)
+    {
+        var request = new GroupRequest { GroupName = groupName };
+        return apiClient.PostAsync<CreateGroupResponse>("api/v1/group/create", request, cancellationToken);
+    }
+
     public Task<CreateGroupResponse> CreateAsync(GroupRequest request, CancellationToken cancellationToken = default)
     {
         const string path = "/api/v1/group/create";
         return apiClient.PostAsync<CreateGroupResponse>(path, request, cancellationToken);
+    }
+
+    #endregion
+
+    #region UpdateAsync
+
+    public Task<LocalApiResponse> UpdateAsync(
+        string groupId,
+        string groupName,
+        CancellationToken cancellationToken = default
+    )
+    {
+        var request = new UpdateGroupRequest { GroupId = groupId, GroupName = groupName };
+        return apiClient.PostAsync<LocalApiResponse>("api/v1/group/update", request, cancellationToken);
     }
 
     public Task<LocalApiResponse> UpdateAsync(UpdateGroupRequest request, CancellationToken cancellationToken = default)
@@ -18,9 +40,15 @@ public class GroupApi(ILocalApiClient apiClient) : IGroupApi
         return apiClient.PostAsync<LocalApiResponse>(path, request, cancellationToken);
     }
 
+    #endregion
+
+    #region ListAsync
+    
     public Task<GroupListResponse> ListAsync(ListGroupsRequest request, CancellationToken cancellationToken = default)
     {
         const string path = "/api/v1/group/list";
         return apiClient.GetAsync<GroupListResponse>(path, request, cancellationToken);
     }
+
+    #endregion
 }
