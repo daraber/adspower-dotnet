9ab6a49ac29ebdee9759ee49fdf5aa2e1e60b910
David <da6003be-s@student.lu.se>
2024-10-13 21:02:35 +0200

    Explicitly set default value
    Avoid future unexpected behavior if default values changes server-side

--- AdsPower.LocalApi/Group/Requests/ListGroupsRequest.cs
+++ AdsPower.LocalApi/Group/Requests/ListGroupsRequest.cs
@@ -15,7 +15,7 @@ public record ListGroupsRequest : IQueryParameterizeable
     /// <summary>
     /// Page number for the query. Defaults to 1.
     /// </summary>
-    public int? Page { get; init; }
+    public int? Page { get; init; } = 1;
 
     /// <summary>
     /// Number of items per page. Defaults to 1, with a maximum of 2000.
