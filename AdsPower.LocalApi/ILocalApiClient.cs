d5013d85d79be015e907f9062c22a21a386893d1
David <da6003be-s@student.lu.se>
2024-10-14 15:43:13 +0200

    Make IQueryParameterizeable parameter nullable

--- AdsPower.LocalApi/ILocalApiClient.cs
+++ AdsPower.LocalApi/ILocalApiClient.cs
@@ -6,7 +6,7 @@ public interface ILocalApiClient
 {
     Task<LocalApiResponse> GetConnectionStatusAsync(CancellationToken cancellationToken = default);
 
-    Task<T> GetAsync<T>(string path, IQueryParameterizeable request, CancellationToken cancellationToken = default)
+    Task<T> GetAsync<T>(string path, IQueryParameterizeable? request, CancellationToken cancellationToken = default)
         where T : LocalApiResponse;
 
     Task<T> PostAsync<T>(string path, object request, CancellationToken cancellationToken = default)
