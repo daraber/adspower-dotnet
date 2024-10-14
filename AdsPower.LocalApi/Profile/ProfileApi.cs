0ce6a2ecc8812e6457137e7ce1bd8c109f742f6b
David <da6003be-s@student.lu.se>
2024-10-14 16:09:51 +0200

    Add overloads

--- AdsPower.LocalApi/Profile/ProfileApi.cs
+++ AdsPower.LocalApi/Profile/ProfileApi.cs
@@ -4,6 +4,9 @@ using AdsPower.LocalApi.Shared;
 
 namespace AdsPower.LocalApi.Profile;
 
+/// <summary>
+/// Provides methods to interact with profile operations.
+/// </summary>
 public class ProfileApi(ILocalApiClient client) : IProfileApi
 {
     public async Task<CreateProfileResponse> CreateAsync(
@@ -24,6 +27,12 @@ public class ProfileApi(ILocalApiClient client) : IProfileApi
         return await client.PostAsync<LocalApiResponse>(path, request, cancellationToken);
     }
 
+    public async Task<ProfilesListResponse> ListAsync(CancellationToken cancellationToken = default)
+    {
+        var request = new ListProfilesRequest();
+        return await ListAsync(request, cancellationToken);
+    }
+
     public async Task<ProfilesListResponse> ListAsync(
         ListProfilesRequest request,
         CancellationToken cancellationToken = default
@@ -31,6 +40,12 @@ public class ProfileApi(ILocalApiClient client) : IProfileApi
     {
         const string path = "/api/v1/user/list";
         return await client.GetAsync<ProfilesListResponse>(path, request, cancellationToken);
+    }
+
+    public async Task<LocalApiResponse> DeleteAsync(List<string> userIds, CancellationToken cancellationToken = default)
+    {
+        var request = new DeleteProfileRequest { UserIds = userIds };
+        return await DeleteAsync(request, cancellationToken);
     }
 
     public async Task<LocalApiResponse> DeleteAsync(
@@ -43,6 +58,16 @@ public class ProfileApi(ILocalApiClient client) : IProfileApi
     }
 
     public async Task<LocalApiResponse> RegroupAsync(
+        List<string> userIds,
+        string groupId,
+        CancellationToken cancellationToken = default
+    )
+    {
+        var request = new RegroupProfilesRequest { UserIds = userIds, GroupId = groupId };
+        return await RegroupAsync(request, cancellationToken);
+    }
+
+    public async Task<LocalApiResponse> RegroupAsync(
         RegroupProfilesRequest request,
         CancellationToken cancellationToken = default
     )
@@ -51,9 +76,7 @@ public class ProfileApi(ILocalApiClient client) : IProfileApi
         return await client.PostAsync<LocalApiResponse>(path, request, cancellationToken);
     }
 
-    public async Task<LocalApiResponse> DeleteCacheAsync(
-        CancellationToken cancellationToken = default
-    )
+    public async Task<LocalApiResponse> DeleteCacheAsync(CancellationToken cancellationToken = default)
     {
         const string path = "/api/v1/user/delete-cache";
         return await client.PostAsync<LocalApiResponse>(path, new { }, cancellationToken);
