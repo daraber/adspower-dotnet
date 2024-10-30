267df689e30a6f771297c01e6872230bce1088e8
David <da6003be-s@student.lu.se>
2024-10-30 23:37:50 +0100

    Add code analysis attribute: MemberNotNullWhen
    Data is expected to not be null when success is true

--- AdsPower.LocalApi/Shared/LocalApiResponse.cs
+++ AdsPower.LocalApi/Shared/LocalApiResponse.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.CodeAnalysis;
 using System.Text.Json.Serialization;
 
 namespace AdsPower.LocalApi.Shared;
@@ -7,11 +8,15 @@ public record LocalApiResponse
     [JsonPropertyName("code")] public required int Code { get; init; }
     [JsonPropertyName("msg")] public required string Message { get; init; }
     
-    public bool Success => Code == 0;
+    [JsonIgnore]
+    public virtual bool Success => Code == 0;
 }
 
 public abstract record LocalApiResponse<T> : LocalApiResponse where T : class
 {
     [JsonPropertyName("data")]
     public abstract T? Data { get; init; }
+
+    [MemberNotNullWhen(true, nameof(Data))]
+    public override bool Success => base.Success && Data is not null;
 }
