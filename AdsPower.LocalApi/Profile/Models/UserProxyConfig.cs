6ab89be687c4e72b514ed980e4786ab6e6ce436c
David <da6003be-s@student.lu.se>
2024-10-30 23:07:12 +0100

    Add explicit JSON properties

--- AdsPower.LocalApi/Profile/Models/UserProxyConfig.cs
+++ AdsPower.LocalApi/Profile/Models/UserProxyConfig.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 namespace AdsPower.LocalApi.Profile.Models;
 
 /// <summary>
@@ -10,45 +12,53 @@ public record UserProxyConfig
     /// Supported options include luminati, lumauto, oxylabsauto, 922S5auto, ipideaauto, ipfoxyauto,
     /// 922S5auth, kookauto, ssh, other, and noproxy.
     /// </summary>
+    [JsonPropertyName("proxy_soft")]
     public required string ProxySoft { get; init; }
 
     /// <summary>
     /// The type of proxy connection. Supported types include http, https, and socks5.
     /// Can be omitted for no_proxy.
     /// </summary>
+    [JsonPropertyName("proxy_type")]
     public string? ProxyType { get; init; }
 
     /// <summary>
     /// The address of the proxy server. Users can enter either a domain name or an IP address.
     /// Can be omitted for no_proxy.
     /// </summary>
+    [JsonPropertyName("proxy_host")]
     public string? ProxyHost { get; init; }
 
     /// <summary>
     /// The port number for the proxy server.
     /// Can be omitted for no_proxy.
     /// </summary>
+    [JsonPropertyName("proxy_port")]
     public string? ProxyPort { get; init; }
 
     /// <summary>
     /// The username for proxy authentication.
     /// </summary>
+    [JsonPropertyName("proxy_user")]
     public string? ProxyUser { get; init; }
 
     /// <summary>
     /// The password for proxy authentication.
     /// </summary>
+    [JsonPropertyName("proxy_password")]
     public string? ProxyPassword { get; init; }
 
     /// <summary>
     /// A URL for changing the proxy IP address, primarily for mobile proxies.
     /// Supported protocols: http, https, and socks5.
     /// </summary>
+    [JsonPropertyName("proxy_url")]
     public string? ProxyUrl { get; init; }
 
     /// <summary>
     /// A flag to indicate whether to manage a list of accounts using the proxy.
     /// 0: No management; 1: Managed.
     /// </summary>
+    [JsonPropertyName("global_config")]
     public int GlobalConfig { get; init; } = 0;
 }
