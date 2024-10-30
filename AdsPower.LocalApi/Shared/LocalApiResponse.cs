522e96ad666362ccc13b283af3e2211a63879b72
David <da6003be-s@student.lu.se>
2024-10-30 23:06:24 +0100

    Add 'success' property

--- AdsPower.LocalApi/Shared/LocalApiResponse.cs
+++ AdsPower.LocalApi/Shared/LocalApiResponse.cs
@@ -6,6 +6,8 @@ public record LocalApiResponse
 {
     [JsonPropertyName("code")] public required int Code { get; init; }
     [JsonPropertyName("msg")] public required string Message { get; init; }
+    
+    public bool Success => Code == 0;
 }
 
 public abstract record LocalApiResponse<T> : LocalApiResponse where T : class
